40531dba8c5409935b268ff97f6c6c5f7913fe06
Ethan Tubia <ethantubia@Ethans-MacBook-Air.local>
2022-12-05 16:23:00 -0500

    Fixed compile errors

--- DayOne/DayOne.java
+++ DayOne/DayOne.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-import java.util.StringBuffer;
 import java.util.ArrayList;
+import java.io.File;
+
 /**
  * Write a description of class DayOne here.
  *
@@ -9,13 +10,13 @@ import java.util.ArrayList;
  */
 public class DayOne
 {
-    public List<Integer> totals = new ArrayList<Integer>();
-    public static List<Integer> calss = new ArrayList<Integer>();
+    public ArrayList<Integer> totals = new ArrayList<Integer>();
+    public static ArrayList<Integer> calss = new ArrayList<Integer>();
     public static void main(String args[]){
         File in = new File("DayOneInputs.txt");
         Scanner scan = new Scanner(in);
-        while (scanner.hasNextInt()){
-            calss.add(scanner.nextInt());
+        while (scanr.hasNextInt()){
+            calss.add(scan.nextInt());
         }
         }
     }
